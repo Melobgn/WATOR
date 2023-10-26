12dcce5809df67363a4740a5e3a3d4a82b5553a6
Antoine MLD <antoine.moulard@hotmail.com>
2023-10-26 11:34:57 +0200

    World créer avec un dataFrame

--- env_world.py
+++ env_world.py
@@ -1,8 +1,12 @@
 import numpy as np
-import random
+import pandas as pd
+
 
 
 # Créer le tableau avec des 0,1,2 aléatoire dans 10 colonnes et 10 lignes
-array = np.random.randint(3, size=(10,10))
-print(array)
-
+df = pd.DataFrame( {  'A' : [0,0,0,0,0,0,0,0,0,0],
+                      'B' : [0,0,0,0,0,0,0,0,0,0],
+                      'C' : [0,0,0,0,0,0,0,0,0,0],
+                      'D' : [0,0,0,0,0,0,0,0,0,0],
+                      'E' : [0,0,0,0,0,0,0,0,0,0]})
+print(df)
