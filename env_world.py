7d98aa322af30026f7ff3cf0c3009f139c9d166b
Antoine MLD <antoine.moulard@hotmail.com>
2023-10-26 11:44:52 +0200

    Préférence avec numpy

--- env_world.py
+++ env_world.py
@@ -4,9 +4,5 @@ import pandas as pd
 
 
 # Créer le tableau avec des 0,1,2 aléatoire dans 10 colonnes et 10 lignes
-df = pd.DataFrame( {  'A' : [0,0,0,0,0,0,0,0,0,0],
-                      'B' : [0,0,0,0,0,0,0,0,0,0],
-                      'C' : [0,0,0,0,0,0,0,0,0,0],
-                      'D' : [0,0,0,0,0,0,0,0,0,0],
-                      'E' : [0,0,0,0,0,0,0,0,0,0]})
-print(df)
+monde = np.zeros((10,10))
+print(monde)
