f017b7c268d36e727f9d36104c6a42d057739710
Cecile HURIAUX <cecile.huriaux@gmail.com>
2023-10-26 21:58:55 +0200

    début class Requins

---
+++ develop.py
@@ -0,0 +1,11 @@
+while:
+class Requins:
+    vies = 0
+    def __init__(self, enregie):
+        self.energie = enregie
+
+    def manger(self):
+        pass
+
+    def mouvement(self):
+        pass
