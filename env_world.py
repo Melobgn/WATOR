29f7dc4af1bcb243a3d8e0cb6951f0167e3bfed8
Antoine MLD <antoine.moulard@hotmail.com>
2023-10-26 16:04:39 +0200

    blocage de la seed des randints

--- env_world.py
+++ env_world.py
@@ -1,37 +1,47 @@
 from random import randint
 import copy
+import random
+
 
 monde = [[0 for i in range(10)] for y in range(10)]
+nombre_poissons = 10
+nombre_requins = 5
 
-
-
+random.seed(12)   
 # Logique des poissons
-count_poissons = 0
-while count_poissons < 10:
+for i in range(nombre_poissons):
+    
     row = randint(0, 9)
     col = randint(0, 9)
     if monde[row][col] == 0:
-        monde[row][col] = 1
-        count_poissons += 1
-            
+            monde[row][col] = 1
+
 # Logique des requins
-count_requins = 0
-while count_requins < 5:
+for i in range(nombre_requins):
     row = randint(0, 9)
     col = randint(0, 9)
     if monde[row][col] == 0:
         monde[row][col] = 2
-        count_requins += 1
 
-# Créer une copie de la grille après l'apparition des poissons et des requins
-monde_initial = copy.deepcopy(monde)
+monde_init = copy.deepcopy(monde)
 
-# print 3 fois la grille pour vérifier si les poissons et requins bougent
-for i in monde_initial:
+    
+# affiche le monde
+for i in monde_init:
     print(*i)
 print()
-for i in monde_initial:
-    print(*i)
-print()
-for i in monde_initial:
-    print(*i)
+
+
+
+
+
+
+
+
+
+      
+        
+
+
+
+
