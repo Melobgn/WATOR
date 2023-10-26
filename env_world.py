a6e9298421e047721aa741a1acd655e655c80f78
Antoine MLD <antoine.moulard@hotmail.com>
2023-10-26 14:49:28 +0200

    Sauvergarde apres apparition des poissons et requins

--- env_world.py
+++ env_world.py
@@ -1,28 +1,37 @@
 from random import randint
-import random
+import copy
 
 monde = [[0 for i in range(10)] for y in range(10)]
 
-# logique des poissons
-count = 0
-while count < 10:
-    row = random.randint(0, 9)  # Génére une ligne aléatoire de 0 à 9
-    col = random.randint(0, 9)  # Génére une colonne aléatoire de 0 à 9
 
+
+# Logique des poissons
+count_poissons = 0
+while count_poissons < 10:
+    row = randint(0, 9)
+    col = randint(0, 9)
     if monde[row][col] == 0:
         monde[row][col] = 1
-        count += 1
-
-# logique des requins
-count = 0
-while count < 5:
-    row = random.randint(0,9)
-    col = random.randint(0,9)
-
+        count_poissons += 1
+            
+# Logique des requins
+count_requins = 0
+while count_requins < 5:
+    row = randint(0, 9)
+    col = randint(0, 9)
     if monde[row][col] == 0:
         monde[row][col] = 2
-        count += 1
+        count_requins += 1
 
-# affiche la grille
-for row in monde:
-    print(row)
+# Créer une copie de la grille après l'apparition des poissons et des requins
+monde_initial = copy.deepcopy(monde)
+
+# print 3 fois la grille pour vérifier si les poissons et requins bougent
+for i in monde_initial:
+    print(*i)
+print()
+for i in monde_initial:
+    print(*i)
+print()
+for i in monde_initial:
+    print(*i)
