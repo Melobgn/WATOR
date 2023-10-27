41d7a7f79114c82f4cbf59fa4276f7c474d0f54f
Melody Bugnon <melodybugnon@gmail.com>
2023-10-27 09:04:40 +0200

    travail hier soir

--- develop.py
+++ develop.py
@@ -1,6 +1,21 @@
+vide = [0][0]
+
+eau = 0
+poisson = 1
+requin = 2
+
+
+class Planete:
+    def __init__(self, pos_x, pos_y)
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+
 class Poisson:
-    def __init__(self, energie, gestation_finie):
-        self.energie = energie
+    def __init__(self, pos_x, pos_y, gestation_finie):
+        self.id_poisson = id_poisson
+        self.pos_x = pos_x
+        self.pos_y = pos_y
         self.gestation_finie = gestation_finie
         gestation_finie = 8
 
@@ -13,17 +28,21 @@ class Poisson:
         if vide:
             if self.gestation_time >= self.gestation_finie:     # if the gestation clock is past due:
                 self.cell.place(Poisson())         #   place a new fish at the current position
-                self.gestation_time = 0 
+                self.gestation_time = 0
+            else: self.gestation_time += 1
 
 
 class Requin(Poisson):
-    def __init__(self, nom, energie, alimentation):
-        super().__init__(nom , energie)
+    def __init__(self, gestation_finie, alimentation, temps_starvation):
+        super().__init__(gestation_finie)
         self.alimentation = alimentation
-
+        self.temps_starvation = temps_starvation
+        temps_starvation = 15
 
     def energie(self):
          if Requin.temps_starvation < 0:
             # Creature dies.
             Requin.dead = True
-            self.grid[Requin.y][Requin.x] = vide
+            self.monde_ready[Requin.row][Requin.col] = vide
+
+
