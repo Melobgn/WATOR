49e414965a439785b9cc79d0255683f8c3675de0
Cecile HURIAUX <cecile.huriaux@gmail.com>
2023-10-27 11:06:36 +0200

    création des instructions Requins

--- develop.py
+++ develop.py
@@ -1,10 +1,48 @@
-class Requins:
-    vies = 0
-    def __init__(self, enregie):
-        self.energie = enregie
+# partie Antoine à supprimer (double)
+# import random
+# from random import randint
 
-    def manger(self):
-        pass
+# génération matrice
+# monde = x = [[randint(0, 0) for i in range(10)] for i in range(10)]
+# for row in monde:
+#     print(*row)
 
-    def mouvement(self):
-        pass
+monde = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 2, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+for row in monde:
+    print(*row)
+
+
+# # partie à conserver Cécile
+# class Requins:
+#     points_de_vie = 6
+#     def __init__(self, id_requin, enregie):
+#         self.id_requin = id_requin
+#         self.energie = enregie
+    
+#     def obtenir_position(self, requin, poisson):
+# # indique la position des requins et des poissons avec x et y
+#         pass
+
+#     def mouvement(self):
+# # vérifie si requin est la même COLONNE que poisson
+
+# # vérifie si requin est la même LIGNE que poisson
+ 
+# # vérifie si position requin et position poisson sont ADJACENTS
+#         pass
+
+
+#     def manger(self):
+# # si position requin et poissont adjacents REQUIN : points_de_vie += 1
+# #                                                   + remplace cellule 1 (poisson) par 2 (requin)
+# #                                                   + cellule 1 est supprimé
+#         pass
