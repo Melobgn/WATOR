2e421b8372485f6001993c44a7bb08692f9d509d
Antoine MLD <antoine.moulard@hotmail.com>
2023-10-27 15:50:23 +0200

    Les requins mangent les poissons

--- env_world.py
+++ env_world.py
@@ -60,28 +60,56 @@ class Planete:
 
     def deplacer_poissons(self):
         deplacement_possible = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    
+
+        poissons_survivants = []  # Nouvelle liste pour les poissons qui ont survécu
+
         for poisson in self.poissons:
             directions_possibles = deplacement_possible[:]
             random.shuffle(directions_possibles)
 
-            deplacement_reussi = False
+            poisson_mange = False
+
+            # Vérifier d'abord si un requin est à proximité
+            for direction in directions_possibles:
+                new_row = poisson['row'] + direction[0]
+                new_col = poisson['col'] + direction[1]
+
+                for requin in self.requins:
+                    if abs(new_row - requin['row']) <= 1 and abs(new_col - requin['col']) <= 1:
+                        self.monde[poisson['row']][poisson['col']] = '\U0001f4a7'  # le poisson est mangé
+                        poisson_mange = True
+                        break
+
+                if poisson_mange:
+                    break
+
+            if not poisson_mange:
+                poissons_survivants.append(poisson)
+
+        # Ne tentez de supprimer les poissons que s'ils ont été mangés
+        if poisson_mange:
+            self.poissons.remove(poisson)
+
+        self.poissons = poissons_survivants  # Mettre à jour la liste des poissons survivants
+
+        # Maintenant, effectuez le déplacement pour les poissons survivants
+        for poisson in poissons_survivants:
+            directions_possibles = deplacement_possible[:]
+            random.shuffle(directions_possibles)
 
             for direction in directions_possibles:
                 new_row = poisson['row'] + direction[0]
                 new_col = poisson['col'] + direction[1]
+
                 if 0 <= new_row < self.longueur and 0 <= new_col < self.largeur:
-                    if self.monde[new_row][new_col] == '\U0001f4a7': #eau
-                        self.monde[poisson['row']][poisson['col']] = '\U0001f4a7' #eau
-                        self.monde[new_row][new_col] = '\U0001f41f' #poisson
+                    if self.monde[new_row][new_col] == '\U0001f4a7':  # eau
+                        self.monde[poisson['row']][poisson['col']] = '\U0001f4a7'  # eau
+                        self.monde[new_row][new_col] = '\U0001f41f'  # poisson
                         poisson['row'] = new_row
                         poisson['col'] = new_col
-                        deplacement_reussi = True
                         break
-            if not deplacement_reussi:
-               
-                return self.monde
-            
+
+
 
     def deplacer_requins(self):
         for requin in self.requins:
