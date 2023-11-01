49ff7e8d8a06dafed659b914054147451c6ffd17
Antoine MLD <antoine.moulard@hotmail.com>
2023-11-01 20:34:46 +0100

    pygame fonctionnel , deplacement, manger_poisson, torus

--- main.py
+++ main.py
@@ -1,33 +1,97 @@
 import pygame
+from simulation import Monde, Poisson, Requin
 
+# Initialisation de Pygame
 pygame.init()
 
-ecran = pygame.display.set_mode((300,200))
+# Création de la fenêtre
+mon_monde = Monde(10, 10, 50)  # Ajustez les dimensions du monde ici
+longueur = mon_monde.longueur * mon_monde.taille_cellule
+hauteur = mon_monde.hauteur * mon_monde.taille_cellule
+
+ecran = pygame.display.set_mode((longueur, hauteur + 80))  # Augmentez la hauteur pour accueillir le texte
 pygame.display.set_caption("Projet Wa-tor")
-blanc = (255, 255, 255)
-ecran.fill(blanc)
 
-# charge les images
-image_poisson = pygame.image.load("poisson_vector.png").convert_alpha()
-image_requin = pygame.image.load("requins_vector.png").convert_alpha()
+# Chargement des images
+image_poisson = pygame.image.load("fish.png").convert_alpha()
+image_requin = pygame.image.load("shark.png").convert_alpha()
 
-# redimensionne les images
-nouvelle_largeur = 70
-nouvelle_hauteur = 70
 
-image_poisson = pygame.transform.scale(image_poisson, (nouvelle_largeur, nouvelle_hauteur))
-image_requin = pygame.transform.scale(image_requin, (nouvelle_largeur, nouvelle_hauteur))
+# Redimensionnement des images
+image_poisson = pygame.transform.scale(image_poisson, (mon_monde.taille_cellule, mon_monde.taille_cellule))
+image_requin = pygame.transform.scale(image_requin, (mon_monde.taille_cellule, mon_monde.taille_cellule))
 
-continuer = True
 
-while continuer:
-    ecran.blit(image_poisson, (0, 50))
-    ecran.blit(image_requin, (100, 100))
+# Couleur du fond
+couleur_fond = (255, 255, 255)
 
+mon_monde.peupler_le_monde(10, 2)
+
+poisson = Poisson(mon_monde)  
+requin = Requin(mon_monde)    
+
+# Boucle principale
+running = True
+chronons = 0
+
+font = pygame.font.Font(None, 25)  # Police et taille du texte
+texte_chronons = font.render(f"Chronons: {chronons}", True, (0, 0, 0))
+texte_requins = font.render(f"Requins: {len(mon_monde.requins)}", True, (0, 0, 0))
+texte_poissons = font.render(f"Poissons: {len(mon_monde.poissons)}", True, (0, 0, 0))
+
+
+
+while running:
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            continuer = False
+        if event.type == pygame.QUIT:
+            running = False  # Quitte le jeu lorsque la fenêtre est fermée
 
+    ecran.fill((255,255,255))  
+
+    # Déplacement des poissons
+    poisson.se_deplacer()
+
+    # Déplacement des requins
+    requin.se_deplacer()
+
+    # Starvation des requins
+    requin.starvation()
+
+    # Manger des poissons
+    requin.manger_poisson()
+
+    # Parcours de la grille et affichage de chaque cellule
+    for i in range(mon_monde.hauteur):
+        for j in range(mon_monde.longueur):
+            cellule = mon_monde.get_cell(j, i)
+            x = j * mon_monde.taille_cellule
+            y = i * mon_monde.taille_cellule
+
+            # Dessin d'une ligne autour de la cellule
+            pygame.draw.rect(ecran, couleur_fond, (x, y, mon_monde.taille_cellule, mon_monde.taille_cellule))
+
+            # Affichage des images de poisson, requin et de la mer
+            if cellule == '\U0001f41f':
+                ecran.blit(image_poisson, (x, y))
+            elif cellule == '\U0001f988':
+                ecran.blit(image_requin, (x, y))
+
+    # Affichage des informations en temps réel
+    texte_chronons = font.render(f"Chronons: {chronons}", True, (0, 0, 0))
+    texte_requins = font.render(f"Requins: {len(mon_monde.requins)}", True, (0, 0, 0))
+    texte_poissons = font.render(f"Poissons: {len(mon_monde.poissons)}", True, (0, 0, 0))
+
+    # Affichage du texte
+    ecran.blit(texte_chronons, (10, hauteur + 10))
+    ecran.blit(texte_requins, (10, hauteur + 40))
+    ecran.blit(texte_poissons, (10, hauteur + 60))
+
+    # Mise à jour de l'affichage
     pygame.display.flip()
 
+    # Incrémentation des chronons
+    chronons += 1
+    pygame.time.delay(800)  # Ajout d'un délai de 800 ms (0.8 seconde) entre les chronons
+
+# Sortie propre
 pygame.quit()
--- simulation.py
+++ simulation.py
@@ -1,25 +1,18 @@
-import os
-import time
 import random
 
-
 class Monde:
-    def __init__(self, longueur, hauteur):
+    def __init__(self, longueur, hauteur, taille_cellule):
         self.longueur = longueur
         self.hauteur = hauteur
+        self.taille_cellule = taille_cellule
         self.monde = [['\U0001f4a7' for _ in range(longueur)] for _ in range(hauteur)]
         self.poissons = []
         self.requins = []
         self.temps_starvation = 8
 
-    def affichage_monde(self):
-        for row in self.monde:
-            print(*row)
-
     def peupler_le_monde(self, nb_poissons, nb_requins):
         self.nb_poissons = nb_poissons
         self.nb_requins = nb_requins
-        # random.seed(12)
         coordonnees_possibles = [(x, y) for x in range(self.longueur) for y in range(self.hauteur)]
         random.shuffle(coordonnees_possibles)
 
@@ -37,10 +30,23 @@ class Monde:
             self.monde[x][y] = '\U0001f988'
             self.requins.append((x, y))
 
+    def get_cell(self, x, y):
+        return self.monde[x % self.longueur][y % self.hauteur]
+
+    def set_cell(self, x, y, value):
+        self.monde[x % self.longueur][y % self.hauteur] = value
+
 
 class Poisson:
     def __init__(self, monde):
         self.monde = monde
+
+    def deplacer_poisson(self, x, y, direction):
+        new_x = x + direction[0]
+        new_y = y + direction[1]
+        self.monde.set_cell(x, y, '\U0001f4a7')
+        self.monde.set_cell(new_x, new_y, '\U0001f41f')
+        return new_x, new_y
 
     def cases_vides_adjacentes(self, x, y):
         deplacement_possible = [(0, 1), (1, 0), (0, -1), (-1, 0)]
@@ -48,21 +54,12 @@ class Poisson:
         cases_vides = []
 
         for dx, dy in deplacement_possible:
-            new_x, new_y = x + dx, y + dy
-            if self.est_dans_le_monde(new_x, new_y) and self.monde.monde[new_x][new_y] == '\U0001f4a7':
+            new_x = x + dx
+            new_y = y + dy
+            if self.monde.get_cell(new_x, new_y) == '\U0001f4a7':
                 cases_vides.append((dx, dy))
 
         return cases_vides
-
-    def deplacer_poisson(self, x, y, direction):
-        new_x = x + direction[0]
-        new_y = y + direction[1]
-        self.monde.monde[x][y] = '\U0001f4a7'
-        self.monde.monde[new_x][new_y] = '\U0001f41f'
-        return new_x, new_y
-
-    def est_dans_le_monde(self, x, y):
-        return 0 <= x < self.monde.longueur and 0 <= y < self.monde.hauteur
 
     def se_deplacer(self):
         new_poissons = []
@@ -73,16 +70,22 @@ class Poisson:
                 new_x, new_y = self.deplacer_poisson(x, y, direction_choisie)
                 new_poissons.append((new_x, new_y))
             else:
-                new_poissons.append((x,y)) # si le poisson ne peut pas bouger, il reste à sa place
+                new_poissons.append((x, y))  # Si le poisson ne peut pas bouger, il reste à sa place
 
         self.monde.poissons = new_poissons
 
-                
+
 class Requin(Poisson):
     def __init__(self, monde):
         super().__init__(monde)
         self.energie = 20
-        
+
+    def deplacer_requin(self, x, y, direction):
+        new_x = x + direction[0]
+        new_y = y + direction[1]
+        self.monde.set_cell(x, y, '\U0001f4a7')
+        self.monde.set_cell(new_x, new_y, '\U0001f988')
+        return new_x, new_y
 
     def cases_avec_un_poisson(self, x, y):
         deplacement_possible = [(0, 1), (1, 0), (0, -1), (-1, 0)]
@@ -91,21 +94,12 @@ class Requin(Poisson):
         cases_avec_poisson = []
 
         for dx, dy in directions_possibles:
-            new_x, new_y = x + dx, y + dy
-            if self.est_dans_le_monde(new_x, new_y) and self.monde.monde[new_x][new_y] == '\U0001f41f':
+            new_x = x + dx
+            new_y = y + dy
+            if self.monde.get_cell(new_x, new_y) == '\U0001f41f':
                 cases_avec_poisson.append((dx, dy))
 
         return cases_avec_poisson
-
-    def deplacer_requin(self, x, y, direction):
-        new_x = x + direction[0]
-        new_y = y + direction[1]
-        self.monde.monde[x][y] = '\U0001f4a7'
-        self.monde.monde[new_x][new_y] = '\U0001f988'
-        return new_x, new_y
-
-    def est_dans_le_monde(self, x, y):
-        return 0 <= x < self.monde.longueur and 0 <= y < self.monde.hauteur
 
     def se_deplacer(self):
         new_requins = []
@@ -116,41 +110,36 @@ class Requin(Poisson):
                 new_x, new_y = self.deplacer_requin(x, y, direction_choisie)
                 new_requins.append((new_x, new_y))
             else:
-                new_requins.append((x,y)) # si le requin ne peut bouger alors il reste à sa position
+                new_requins.append((x, y))  # Si le requin ne peut pas bouger alors il reste à sa position
         self.monde.requins = new_requins
-
 
     def starvation(self):
         requins_a_retirer = []
 
         for x, y in self.monde.requins:
             if self.energie <= 0:  # Si l'énergie est épuisée
-                self.monde.monde[x][y] = '\U0001f4a7'  # Mettre de l'eau à la place du requin
+                self.monde.set_cell(x, y, '\U0001f4a7')  # Mettre de l'eau à la place du requin
                 requins_a_retirer.append((x, y))
             else:
-                self.energie -= 0.5 # Réduire l'énergie du requin à chaque tour
+                self.energie -= 0.5  # Réduire l'énergie du requin à chaque tour
 
         for requin in requins_a_retirer:
             self.monde.requins.remove(requin)
 
-    
     def manger_poisson(self):
         poissons_a_retirer = []
 
         for requin_x, requin_y in self.monde.requins:
             for poisson_x, poisson_y in self.monde.poissons:
-                if (abs(requin_x - poisson_x) <= 1 and abs(requin_y - poisson_y) <= 1) and (abs(requin_x - poisson_x) + abs(requin_y - poisson_y) == 1):
-                    self.monde.monde[poisson_x][poisson_y] = '\U0001f4a7'
+                if abs(requin_x - poisson_x) <= 1 and abs(requin_y - poisson_y) <= 1:
+                    self.monde.set_cell(poisson_x, poisson_y, '\U0001f4a7')
                     self.energie += 10
                     poissons_a_retirer.append((poisson_x, poisson_y))
 
         for poisson in poissons_a_retirer:
             self.monde.poissons.remove(poisson)
 
-
-    
     def reproduction(self):
-        self.gestation = 0
         poissons_a_ajouter = []
         for x, y in self.monde.poissons[:]:
             if self.gestation == self.monde.temps_reproduction_poisson:
@@ -160,31 +149,7 @@ class Requin(Poisson):
                     new_x, new_y = self.deplacer_poisson(x, y, direction_choisie)
                     poissons_a_ajouter.append((new_x, new_y))
                     self.gestation = 0
-                    self.monde.monde[new_x][new_y] = '\U0001f41f'  # Mettez à jour la grille avec le bébé poisson
+                    self.monde.set_cell(new_x, new_y, '\U0001f41f')  # Mettez à jour la grille avec le bébé poisson
             else:
                 self.gestation += 1
         self.monde.poissons.extend(poissons_a_ajouter)
-
-    
-
-chronons = 0
-mon_monde = Monde(10, 10)
-deplacement_poisson = Poisson(mon_monde)
-deplacement_requin = Requin(mon_monde)
-mon_monde.peupler_le_monde(10,2)
-mon_monde.affichage_monde()
-
-#Compteur chronons
-while chronons < 100:
-    os.system('clear')
-    deplacement_poisson.se_deplacer()
-    deplacement_requin.se_deplacer()
-    
-    deplacement_requin.starvation()
-    deplacement_requin.manger_poisson()
-    mon_monde.affichage_monde()
-    print(f"Nombre de requins : {mon_monde.nb_requins}, Nombre de poissons : {mon_monde.nb_poissons}")
-    print(f"Requins dans l'océan : {len(mon_monde.requins)}, poissons dans l'océan : {len(mon_monde.poissons)}")
-    chronons += 1
-    time.sleep(0.8)
-    
