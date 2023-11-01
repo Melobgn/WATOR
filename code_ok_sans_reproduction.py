27f9578a70cbd08fdcffce88dbe17d613d476b8c
Melobgn <147378543+Melobgn@users.noreply.github.com>
2023-11-01 21:48:48 +0100

    Update code_ok_sans_reproduction.py

--- code_ok_sans_reproduction.py
+++ code_ok_sans_reproduction.py
@@ -10,7 +10,6 @@ class Monde:
         self.monde = [['\U0001f4a7' for _ in range(longueur)] for _ in range(hauteur)]
         self.poissons = []
         self.requins = []
-        self.temps_starvation = 8
 
     def affichage_monde(self):
         for row in self.monde:
@@ -28,148 +27,139 @@ class Monde:
                 break
             x, y = coordonnees_possibles.pop()
             self.monde[x][y] = '\U0001f41f'
-            self.poissons.append((x, y))
+            poiscaille = Poisson(self, x, y)
+            self.poissons.append(poiscaille)
 
         for _ in range(self.nb_requins):
             if not coordonnees_possibles:
                 break
             x, y = coordonnees_possibles.pop()
             self.monde[x][y] = '\U0001f988'
-            self.requins.append((x, y))
+            requinx = Requin(self, x, y)
+            self.requins.append(requinx)
 
 
 class Poisson:
-    def __init__(self, monde):
+    def __init__(self, monde, x, y):
         self.monde = monde
+        self.x = x
+        self.y = y
+        self.temps_de_reproduction = 8
 
-    def cases_vides_adjacentes(self, x, y):
+    def cases_vides_adjacentes(self):
         deplacement_possible = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         random.shuffle(deplacement_possible)
         cases_vides = []
 
         for dx, dy in deplacement_possible:
-            new_x, new_y = x + dx, y + dy
-            if self.est_dans_le_monde(new_x, new_y) and self.monde.monde[new_x][new_y] == '\U0001f4a7':
+            new_x = (self.x + dx) % self.monde.longueur
+            new_y = (self.y + dy) % self.monde.hauteur
+            if self.monde.monde[new_x][new_y] == '\U0001f4a7':
                 cases_vides.append((dx, dy))
 
         return cases_vides
 
-    def deplacer_poisson(self, x, y, direction):
-        new_x = x + direction[0]
-        new_y = y + direction[1]
-        self.monde.monde[x][y] = '\U0001f4a7'
-        self.monde.monde[new_x][new_y] = '\U0001f41f'
-        return new_x, new_y
-
-    def est_dans_le_monde(self, x, y):
-        return 0 <= x < self.monde.longueur and 0 <= y < self.monde.hauteur
-
-    def se_deplacer(self):
-        new_poissons = []
-        for x, y in self.monde.poissons:
-            cases_vides = self.cases_vides_adjacentes(x, y)
-            if cases_vides:
-                direction_choisie = random.choice(cases_vides)
-                new_x, new_y = self.deplacer_poisson(x, y, direction_choisie)
-                new_poissons.append((new_x, new_y))
-            else:
-                return None
-
-        self.monde.poissons = new_poissons
+    
+    def deplacer_poisson(self):
+        cases_vides = self.cases_vides_adjacentes()
+        if cases_vides:
+            direction_choisie = random.choice(cases_vides)
+            dx, dy = direction_choisie
+            new_x = (self.x + dx) % self.monde.longueur
+            new_y = (self.y + dy) % self.monde.hauteur
+            self.monde.monde[self.x][self.y] = '\U0001f4a7'
+            self.monde.monde[new_x][new_y] = '\U0001f41f'
+            self.x, self.y = new_x, new_y
+        else:
+            new_x, new_y = self.x, self.y  # Ne pas bouger
+            self.monde.monde[new_x][new_y] = '\U0001f41f'
 
                 
 class Requin(Poisson):
-    def __init__(self, monde):
-        super().__init__(monde)
-        self.energie = 20
-        
+    def __init__(self, monde, x, y):
+        super().__init__(monde, x, y)
+        self.energie = 12
 
-    def cases_vides_adjacentes(self, x, y):
-        deplacement_possible = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        directions_possibles = deplacement_possible[:]
-        random.shuffle(directions_possibles)
-        cases_vides = []
 
-        for dx, dy in directions_possibles:
-            new_x, new_y = x + dx, y + dy
-            if self.est_dans_le_monde(new_x, new_y) and self.monde.monde[new_x][new_y] == '\U0001f4a7':
-                cases_vides.append((dx, dy))
+    def deplacer_requin(self):
+        cases_vides = self.cases_vides_adjacentes()
+        poisson_proche = None
 
-        return cases_vides
+        for poiscaille in self.monde.poissons:
+            if abs(self.x - poiscaille.x) <= 1 and abs(self.y - poiscaille.y) <= 1:
+                poisson_proche = poiscaille
 
-    def deplacer_requin(self, x, y, direction):
-        new_x = x + direction[0]
-        new_y = y + direction[1]
-        self.monde.monde[x][y] = '\U0001f4a7'
-        self.monde.monde[new_x][new_y] = '\U0001f988'
-        return new_x, new_y
-
-    def est_dans_le_monde(self, x, y):
-        return 0 <= x < self.monde.longueur and 0 <= y < self.monde.hauteur
-
-    def se_deplacer(self):
-        new_requins = []
-        for x, y in self.monde.requins:
-            cases_vides = self.cases_vides_adjacentes(x, y)
-            if cases_vides:
-                direction_choisie = random.choice(cases_vides)
-                new_x, new_y = self.deplacer_requin(x, y, direction_choisie)
-                new_requins.append((new_x, new_y))
+        if poisson_proche:
+            # Si un poisson est proche, se déplacer vers lui
+            if self.x < poisson_proche.x:
+                new_x, new_y = self.x + 1, self.y
+            elif self.x > poisson_proche.x:
+                new_x, new_y = self.x - 1, self.y
+            elif self.y < poisson_proche.y:
+                new_x, new_y = self.x, self.y + 1
             else:
-                return None
-        self.monde.requins = new_requins
-
+                new_x, new_y = self.x, self.y - 1
+            self.monde.monde[self.x][self.y] = '\U0001f4a7'
+            self.monde.monde[new_x][new_y] = '\U0001f988'
+            self.x, self.y = new_x, new_y
+        elif cases_vides:
+            # Si aucun poisson proche, se déplacer vers une case vide aléatoire
+            direction_choisie = random.choice(cases_vides)
+            dx, dy = direction_choisie
+            new_x = (self.x + dx) % self.monde.longueur
+            new_y = (self.y + dy) % self.monde.hauteur
+            self.monde.monde[self.x][self.y] = '\U0001f4a7'
+            self.monde.monde[new_x][new_y] = '\U0001f988'
+            self.x, self.y = new_x, new_y
+        else:
+            self.x, self.y = self.x, self.y  # Ne pas bouger
+            self.monde.monde[self.x][self.y] = '\U0001f988'  # Mettre l'emoji requin sur la case actuelle
 
     def starvation(self):
-        requins_a_retirer = []
+        if self.energie <= 0:
+            self.monde.monde[self.x][self.y] = '\U0001f4a7'
+            self.monde.requins.remove(self)
+        else:
+            self.energie -= 1
 
-        for x, y in self.monde.requins:
-            if self.energie <= 0:  # Si l'énergie est épuisée
-                self.monde.monde[x][y] = '\U0001f4a7'  # Mettre de l'eau à la place du requin
-                requins_a_retirer.append((x, y))
-            else:
-                self.energie -= 1 # Réduire l'énergie du requin à chaque tour
-
-        for requin in requins_a_retirer:
-            self.monde.requins.remove(requin)
-
-    
     def manger_poisson(self):
-        poissons_a_retirer = []
-
-        for requin_x, requin_y in self.monde.requins:
-            for poisson_x, poisson_y in self.monde.poissons:
-                if (abs(requin_x - poisson_x) <= 1 and abs(requin_y - poisson_y) == 0) or (abs(requin_x - poisson_x) == 0 and abs(requin_y - poisson_y) <= 1):
-                    self.monde.monde[poisson_x][poisson_y] = '\U0001f4a7'
-                    self.energie += 5
-                    poissons_a_retirer.append((poisson_x, poisson_y))
-
-        for poisson in poissons_a_retirer:
-            self.monde.poissons.remove(poisson)
-
-
-    
-    # def deplacer_poisson(self, poisson, direction):
-    #     return super().deplacer_poisson(poisson, direction)
-
-    
+        for poiscaille in self.monde.poissons:
+            if (abs(self.x - poiscaille.x) <= 1 and abs(self.y - poiscaille.y) == 0) or (abs(self.x - poiscaille.x) == 0 and abs(self.y - poiscaille.y) <= 1):
+                self.monde.monde[poiscaille.x][poiscaille.y] = '\U0001f4a7'
+                self.energie += 2
+                self.monde.poissons.remove(poiscaille)
+ 
 
 chronons = 0
 mon_monde = Monde(10, 10)
-deplacement_poisson = Poisson(mon_monde)
-deplacement_requin = Requin(mon_monde)
-mon_monde.peupler_le_monde(10,5)
+poissons = mon_monde.poissons
+requins = mon_monde.requins
+mon_monde.peupler_le_monde(7,3)
 mon_monde.affichage_monde()
+print()
 
 #Compteur chronons
-while chronons < 100:
-    os.system('clear')
-    deplacement_poisson.se_deplacer()
-    deplacement_requin.se_deplacer()
-    deplacement_requin.starvation()
-    deplacement_requin.manger_poisson()
+while chronons < 50:
+    #os.system('cls')
+
+    # Faites se déplacer chaque poisson
+    for poiscaille in mon_monde.poissons:
+        poiscaille.deplacer_poisson()
+
+    # Faites se déplacer chaque requin
+    for requinx in mon_monde.requins:
+        requinx.deplacer_requin()
+        requinx.manger_poisson()
+        requinx.starvation()
+
+    for poiscaille in mon_monde.poissons:
+        mon_monde.monde[poiscaille.x][poiscaille.y] = '\U0001f41f'
+
+    for requinx in mon_monde.requins:
+        mon_monde.monde[requinx.x][requinx.y] = '\U0001f988'
+
     mon_monde.affichage_monde()
     print()
     chronons += 1
-    time.sleep(0.8)
+    time.sleep(4)
     
