69781e5754d14a9e5c4c3a874d86e865c77278b1
Antoine MLD <antoine.moulard@hotmail.com>
2023-11-02 14:45:19 +0100

    Factorisation deplacement requin

--- simulation.py
+++ simulation.py
@@ -96,11 +96,30 @@ class Requin(Poisson):
 
 
     def deplacer_requin(self):
-        poisson_proche = None
         cases_vides = self.cases_vides_adjacentes()
         if not cases_vides:
             self.gestation += 1
             return
+        elif cases_vides:
+            self.cases_poissons_miam()
+        if not self.cases_poissons_miam():
+            if cases_vides:
+            # Si aucun poisson proche, se déplacer vers une case vide aléatoire
+                direction_choisie = random.choice(cases_vides)
+                dx, dy = direction_choisie
+                new_x = (self.x + dx) % self.monde.longueur
+                new_y = (self.y + dy) % self.monde.hauteur
+                if self.gestation >= self.temps_de_reproduction:
+                    self.reproduction_requin()
+                    self.gestation = 0
+                else:
+                    self.monde.grille[self.x][self.y] = 0
+                    self.monde.grille[new_x][new_y] = '\U0001f988'
+                    self.x, self.y = new_x, new_y
+                    self.gestation += 1
+        
+    def cases_poissons_miam(self):
+        poisson_proche = None
         for poiscaille in self.monde.poissons:
             if abs(self.x - poiscaille.x) <= 1 and abs(self.y - poiscaille.y) <= 1:
                 poisson_proche = poiscaille
@@ -114,21 +133,6 @@ class Requin(Poisson):
                 new_x, new_y = self.x, self.y + 1
             else:
                 new_x, new_y = self.x, self.y - 1
-            if self.gestation >= self.temps_de_reproduction:
-                self.reproduction_requin()
-                self.gestation = 0
-            else:
-                self.monde.grille[self.x][self.y] = 0
-                self.monde.grille[new_x][new_y] = '\U0001f988'
-                self.x, self.y = new_x, new_y
-                self.gestation += 1
-
-        elif cases_vides:
-            # Si aucun poisson proche, se déplacer vers une case vide aléatoire
-            direction_choisie = random.choice(cases_vides)
-            dx, dy = direction_choisie
-            new_x = (self.x + dx) % self.monde.longueur
-            new_y = (self.y + dy) % self.monde.hauteur
             if self.gestation >= self.temps_de_reproduction:
                 self.reproduction_requin()
                 self.gestation = 0
@@ -151,7 +155,7 @@ class Requin(Poisson):
         for poiscaille in self.monde.poissons:
             if (abs(self.x - poiscaille.x) <= 1 and abs(self.y - poiscaille.y) == 0) or (abs(self.x - poiscaille.x) == 0 and abs(self.y - poiscaille.y) <= 1):
                 self.monde.grille[poiscaille.x][poiscaille.y] = 0
-                self.energie += 4
+                self.energie += 2
                 self.monde.poissons.remove(poiscaille)
 
     def reproduction_requin(self):
@@ -222,6 +226,11 @@ class Simulation:
                 requinx.deplacer_requin()
                 requinx.starvation()
                 requinx.manger_poisson()
+            for poiscaille in self.monde.poissons:
+                self.monde.grille[poiscaille.x][poiscaille.y] = '\U0001f41f' #poisson
+
+            for requinx in self.monde.requins:
+                self.monde.grille[requinx.x][requinx.y] = '\U0001f988' #requin
 
             # Dessin des cellules
             for i in range(self.monde.hauteur):
