f98dd4b1a577967a96972fcaee39e49354c33b80
Antoine <antoine.moulard@hotmail.com>
2023-11-06 15:51:14 +0100

    fini

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import sys
 
 
 def main():
-    pygame.init()
-    world = World(50,50)
-    simulation = Simulation(world,1000, 40, 10, 15)
+    world = World(50, 50)
+    simulation = Simulation(world, 1000, 40, 10, 15)
     simulation.initialisation()
 
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
     simulation.ending_simulation()
-    pygame.quit()
-    sys.exit()
 
 if __name__ == "__main__":
     main()
